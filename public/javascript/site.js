c03a63ec805bcb6fdd46401029360884970ee8f2
Blake Sweeney <blakes.85@gmail.com>
2011-08-23 09:54:36 -0400

    Simple working version

--- public/javascript/site.js
+++ public/javascript/site.js
@@ -1,4 +1,3 @@
-// Parser for position columns
 $.tablesorter.addParser({
   id: 'positions',
   is: function(s) { return false; },
@@ -7,13 +6,21 @@ $.tablesorter.addParser({
 });
 
 $(document).ready(function() {
-  $("#position-table").tablesorter({ });
+
+  $("#position-table").tablesorter({ sortList: [[0, 0]] });
+
   $(".load_analysis_image").click(function(){
     var sibs = $(this).parent().siblings();
     var to_text = function(n) { return n.firstChild.data };
     var pos = to_text(sibs[0]) + "-" + to_text(sibs[1]);
     var family = sibs[3].children[0].text;
     var img_src = '/images/positions/' + family + '_' + pos + '.png';
+    $("#analysis_image").attr('src', img_src);
+  });
+
+  $(".load_family_image").click(function(){
+    var family = $(this).text();
+    var img_src = '/images/families/' + family + '.png';
     $("#analysis_image").attr('src', img_src);
   });
 });
--- site.rb
+++ site.rb
@@ -1,8 +1,6 @@
 require "bundler"
 Bundler.require
 
-Struct.new("Interaction", :positions, :sequence, :family)
-
 $db = Mongo::Connection.new.db('biology')
 $families = %w{cWW tWW cWH tWH cWS tWS cHH tHH cHS tHS tSS cSS}.freeze
 
@@ -11,12 +9,12 @@ get '/' do
   query = Plucky::Query.new($db['interactions'])
   @data = query.all(
     :family.in => $families,
+    :conserved => true,
     :pdb => '2AW7')
   haml :index
 end
 
 get '/family/?' do
-  # SELECT family, COUNT(
   @families = $db['interactions'].group(
     :initial => '{}',
     :reduce => 'function(obj, agg) {
